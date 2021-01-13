d1488bf1a6ab17f66eb4d3c7b0e3ac1a917f7efc
shipui2005 <32000065+shipui2005@users.noreply.github.com>
2021-01-13 00:51:06 -0500

    Update CurvatureModelDemo.m

--- CurvatureModelDemo.m
+++ CurvatureModelDemo.m
@@ -5,8 +5,39 @@
 load('bananaBank.mat');
 addpath('./');
 
-%% Load the image
-img = imread('./SampleImages/scene.jpeg');
 
-%% Obtain curvature rating and distribution of the image
-[rating, dist] = curveRate(img, bf);
+%% Load the image and obtain curvature rating and distribution of the image
+
+img = imread('./SampleImages/square.jpeg'); % load the image
+figure
+imshow(img);
+
+% The function will resize the image into  256*256, this process may have
+% effect on the curvature rating if the aspect ratio of the input image is different
+[rating_square, ~] = curveRate(img, bf); % obtain curvature rating
+
+
+% The rating of the circle image should have higher curvature rating
+% than the square one
+img = imread('./SampleImages/circle.png'); % load the image
+
+figure
+imshow(img);
+[rating_circle, ~] = curveRate(img, bf); % obtain curvature rating
+
+% A house example, the chosen curvature filter bank works better in natural
+% images 
+img = imread('./SampleImages/house.jpeg'); % load the image
+figure
+imshow(img);
+[rating_house, ~] = curveRate(img, bf); % obtain curvature rating
+
+% An animal example, this default model works better when edge pixels could
+% be easily captured by the MATLAB default edge detector, see the edgemap
+img = imread('./SampleImages/animal.png'); % load the image
+edgeMap = edge(rgb2gray(img),'Roberts');
+figure
+imshow(img);% show orignal image
+figure
+imshow(edgeMap); % show edge map
+[rating_animal, ~] = curveRate(img, bf); % obtain curvature rating
